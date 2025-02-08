faf8485ec371afaadbb508f7d2b6466a3cf6f56f
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-02-08 17:19:04 +0700

    props-part3

--- 14.0 props-start/src/App.jsx
+++ 14.0 props-start/src/App.jsx
@@ -1,37 +1,6 @@
-import PropTypes from "prop-types";
 import { myData } from "../data.js";
-import logo from "./assets/logo-tuhoc.png";
-
-function Header() {
-  return (
-    <>
-      <header>
-        <img src={logo} alt="Tự Học" />
-        <h1>React Tuhoc.cc</h1>
-        <p>
-          Học React - Khám phá cách xây dựng ứng dụng linh hoạt, hiện đại, và
-          đầy sáng tạo!
-        </p>
-      </header>
-    </>
-  );
-}
-
-function MainContent({ image, title, desc }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{desc}</p>
-    </li>
-  );
-}
-
-MainContent.propTypes = {
-  image: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  desc: PropTypes.string,
-};
+import Header from "./components/Header/Header.jsx";
+import MainContent from "./components/Header/MainContent.jsx";
 
 function App() {
   return (
---
+++ 14.0 props-start/src/components/Header/MainContent.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+MainContent.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string,
+};
+
+function MainContent({ image, title, desc }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </li>
+  );
+}
+
+export default MainContent;
