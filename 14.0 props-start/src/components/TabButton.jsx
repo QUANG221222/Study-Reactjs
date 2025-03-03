f8d2f72197b07ee6b1f6ea037568800d52eef63b
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-03-03 19:37:04 +0700

    14.14 React js - Thêm Class Active Cho button

--- 14.0 props-start/src/components/TabButton.jsx
+++ 14.0 props-start/src/components/TabButton.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 //Cách 1
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   //js
   // const btn = document.querySelector("button");
   // btn.addEventListener("click", () => {
@@ -8,12 +8,16 @@ export default function TabButton({ children, onSelect }) {
   // });
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
 TabButton.propTypes = {
   children: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool,
 };
 //Cách 2
 // export default function TabButton({ batky }) {
