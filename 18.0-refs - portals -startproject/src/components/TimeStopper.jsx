492b97d74d1d946927e2f6fdaff9ce3249b1931c
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-07-22 11:25:12 +0700

    18.5 Thêm useState Timer- Refs & Portals trong React - Dự án thực hành react refs và protals

--- 18.0-refs - portals -startproject/src/components/TimeStopper.jsx
+++ 18.0-refs - portals -startproject/src/components/TimeStopper.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function TimeStopper({ title, target }) {
+  const [timerStart, setTimerStart] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const handleStart = () => {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, target * 1000);
+    setTimerStart(true);
+  };
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>Bạn đã thua</p>}
       <p className="challenge-time">
         {target} second {target > 0 ? "s" : ""}
       </p>
-      <button>Start</button>
-      <p className="">Time is running.../Timer inactive</p>
+      <button onClick={handleStart}>{timerStart ? "Stop" : "Start"}</button>
+      <p className={timerStart ? "active" : ""}>
+        {timerStart ? "Timer is running" : "Timer inactive"}
+      </p>
     </section>
   );
 }
